979a97670362cf738c6d7a3b44f027b525cf1863
Mark Brocato <mark.brocato@sencha.com>
2017-09-11 20:05:24 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Switch, BrowserRouter, Route, browserHistory, Link } from 'react-router-dom';
+import { Container, Panel } from '@extjs/ext-react';
+
+Ext.require('layout.fit')
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Container fullscreen layout="fit">
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/address' component={Address} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Container>
+            </BrowserRouter>
+
+        )
+    }
+}
+
+const Home = () => (
+    <Panel title="Home" bodyPadding={20}>
+        The home view.
+        <Link to="/address">Address View</Link>
+    </Panel>
+);
+
+const Address = () => (
+    <Panel title="Address" bodyPadding={20}>
+        The address view.
+        <Link to="/">Home View</Link>
+    </Panel>
+);
+
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtReactWebpackPlugin = require('@extjs/reactor-webpack-plugin')
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                options: {
+                    plugins: [
+                        '@extjs/reactor-babel-plugin'
+                    ]
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtReactWebpackPlugin({
+
+        }),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+
+    stats: {
+        errorDetails: true
+    },
+
+    devServer: {
+        contentBase: './build',
+        historyApiFallback: true
+    }
+};
